889a48f9524e8c061797c4f7a0832079f861de3c
mt-btanner <btanner@moneytree.jp>
2024-12-02 10:55:14 +0900

    Ensure prettier actually runs, and on save

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -88,10 +88,16 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				javascript = { "prettierd", "prettier", stop_after_first = true },
-				format_on_save = {
-					timeout_ms = 500,
-					lsp_format = "fallback",
-				},
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+				html = { "prettierd", "prettier", stop_after_first = true },
+				css = { "prettierd", "prettier", stop_after_first = true },
+				json = { "prettierd", "prettier", stop_after_first = true },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
 			},
 		},
 	},
